0c47f2b1b0639997dba869d2ade85f38a4c3bba1
mshuber1981 <michael.huber1981@outlook.com>
2021-08-25 07:11:29 -0500

    Removing Portfolio project files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./assets/scss/index.scss";
-import App from "./assets/containers/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
